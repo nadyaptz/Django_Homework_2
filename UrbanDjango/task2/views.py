723a591795dd80aae7be9555034214403cb63fdb
Надя <nadya.ptz@gmail.com>
2025-01-15 18:44:24 +0300

    task2

---
+++ UrbanDjango/task2/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+# python manage.py runserver
+
+# Create your views here.
+
+def func_view(request):
+    return render(request, 'func_template.html')
+
+
+class class_view(TemplateView):
+    template_name = 'class_template.html'
